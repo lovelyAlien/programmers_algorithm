901ffc04851f7b6d5b2b00e8e6326c8afa5e2d1c
Jaeseung Choun <54395509+lovelyAlien@users.noreply.github.com>
2021-01-04 10:30:11 +0900

    Correct : H-Index
    프로그래머스 H-Index 정답 소스코드

---
+++ Sort/H-Index/sol.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//이진탐색
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+    int l = 0; 
+    int r = citations.size();
+    while(l<=r){
+        int mid= (l+r) /2;
+        int cnt=0;
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]>=mid) cnt++;
+            else break;
+        }
+        
+        if(cnt>=mid){
+            answer=mid;
+            l=mid+1;
+        }
+        else 
+            r=mid-1;
+    }
+   
+    return answer;
+}
